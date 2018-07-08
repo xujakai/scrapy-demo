2c90d80af2d08a1f26b30bff3a5cbbe7458ca004
xjk <xujakai@163.com>
2018-07-08 14:57:46 +0800

    mysql整合 2018年7月8日14:57:43

--- demo/settings.py
+++ demo/settings.py
@@ -65,9 +65,11 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#     'demo.pipelines.DemoPipeline': 300,
-# }
+# 指定用来处理数据的 Pipeline 类，后面的数字代表执行顺序,取值范围是  0-1000 range.
+# 数值小的 Pipeline 类优先执行
+ITEM_PIPELINES = {
+    'demo.pipelines.DemoPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
